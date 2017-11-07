c2cb240e8aa41304377ae451a07a1fa40f06b8ed
exzackly <exzackly7@me.com>
2017-11-07 13:37:38 -0500

    Update to new domain

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Marist sub-second automatic registration script
 
 INSTALLATION - Using Greasemonkey on Firefox or Tampermonkey on Chrome, create a new script, then copy the script into it
  
-USAGE – Enter your CRN numbers between the [] on line 15. Choose the time to start the sniper (typically 1 minute before registration) on line 28; follow the format described in line 27. Enable the script, and go to the Student tab in Banner Self Service, then click Add or Drop Classes. The script will execute at the selected time
+USAGE – Choose the time to start the sniper (typically 1 minute before registration) on line 13; follow the format described on line 12. Enter your CRNs (separated by a comma) between the [] on line 14. Enable the script, go to the Student tab in Banner Self Service, then click Add or Drop Classes. The script will execute at the selected time
 
 JavaScript
--- maristRegistrationSniper.js
+++ maristRegistrationSniper.js
@@ -3,27 +3,30 @@
 // @author       EXZACKLY
 // @version      7
 // @description  Marist sub-second automatic registration script
-// @include      https://ssb.banner.marist.edu/*
+// @include      https://ssb1.banner.marist.edu*
 // @copyright    EXZACKLY INC
 // ==/UserScript==
 
+var domain = "https://ssb1.banner.marist.edu:8012/MARPROD/";
+// Indicate time to fire script (typically 1 minute before registration begins)
+// Year (YYYY), Month (0-11), Date, Hour (0-23), Minute
+var fireTime = new Date(2017, 10, 7, 6, 59);
+var CRNs = []; // Enter CRNS into array; separate CRNs with commas
+
 var fireRegistrationSniper = function() {
-    if (window.location == "https://ssb.banner.marist.edu/PROD11G/bwskfreg.P_AltPin") {
+    if (window.location == (domain + "bwskfreg.P_AltPin")) {
         if (document.getElementById("crn_id1") === null) {
             location.reload();
         } else {
-            var crns = []; // Enter CRNS into array; separate CRNs with commas
-            for (var temp = 0; temp < crns.length; temp++) {
-                document.getElementById("crn_id" + (temp + 1)).value = crns[temp];
+            for (var i = 0; i < CRNs.length; i++) {
+                document.getElementById("crn_id" + (i + 1)).value = CRNs[i];
             }
             document.getElementsByName("REG_BTN")[1].click();
         }
-    } else if (window.location == "https://ssb.banner.marist.edu/PROD11G/bwckcoms.P_Regs") {
+    } else if (window.location == (domain + "bwckcoms.P_Regs")) {
         alert("Registration Complete!");
     }
 };
 
-// Indicate time to fire script (typically 1 minute before registration begins)
-// Year (YYYY), Month (0-11), Date, Hour (0-23), Minute
-var timeUntil = new Date(2015, 3, 2, 0, 13).getTime() - Date.now();
+var timeUntil = fireTime.getTime() - Date.now();
 setTimeout(fireRegistrationSniper, timeUntil);
